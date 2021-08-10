1e91d179e321460e50fa7f8a9813f70eeb5483db
Arykin Valera <muzungu.av@gmail.com>
2021-08-10 11:26:13 +0300

    error response & exeptions

---
+++ src/main/java/ru/avv/test/unikoomapp/exception/ErrorResponse.java
@@ -0,0 +1,31 @@
+package ru.avv.test.unikoomapp.exception;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ErrorResponse {
+
+    @JsonProperty("error")
+    private String error;
+
+    @JsonProperty("error_description")
+    private String errorDescription;
+
+    public ErrorResponse() {
+
+    }
+
+    public ErrorResponse(String error, String errorDescription) {
+        super();
+        this.error = error;
+        this.errorDescription = errorDescription;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getErrorDescription() {
+        return errorDescription;
+    }
+
+}
---
+++ src/main/java/ru/avv/test/unikoomapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,60 @@
+package ru.avv.test.unikoomapp.exception;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.persistence.NoResultException;
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = {NoResultException.class})
+    @ResponseBody
+    ResponseEntity<ErrorResponse> handleFileUploadException(HttpServletRequest request, NoResultException ex) {
+        ErrorResponse response = new ErrorResponse("Empty Result", "No entity found for query.");
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = {NumberFormatException.class})
+    @ResponseBody
+    ResponseEntity<ErrorResponse> handleNumberFormatException(HttpServletRequest request, NumberFormatException ex) {
+        ErrorResponse response = new ErrorResponse("Number Format Error", "Number Format Error " + ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(value = {InvalidFormatException.class})
+    @ResponseBody
+    ResponseEntity<ErrorResponse> handleInvalidFormatException(HttpServletRequest request, InvalidFormatException ex) {
+        ErrorResponse response = new ErrorResponse("Invalid Format", "Invalid Format " + ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    @ResponseBody
+    ResponseEntity<ErrorResponse> handleIllegalArgumentException(HttpServletRequest request, IllegalArgumentException ex) {
+        ErrorResponse response = new ErrorResponse("Illegal Argument", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(value = {NullPointerException.class})
+    @ResponseBody
+    ResponseEntity<ErrorResponse> handleNullPointerException(HttpServletRequest request, NullPointerException ex) {
+        ErrorResponse response = new ErrorResponse("Something is Null", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = {HttpMessageNotReadableException.class})
+    @ResponseBody
+    ResponseEntity<ErrorResponse> handleNullPointerException(HttpServletRequest request, HttpMessageNotReadableException ex) {
+        ErrorResponse response = new ErrorResponse("Something that doesn't look like valid JSON.", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
