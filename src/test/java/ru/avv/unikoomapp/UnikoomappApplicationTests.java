85679d10825b0a9cb15b2e3dd3b233b26d78f7ec
Arykin Valera <muzungu.av@gmail.com>
2022-05-19 13:45:29 +0500

    fix README

---
+++ src/test/java/ru/avv/unikoomapp/UnikoomappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.avv.unikoomapp;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UnikoomappApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
